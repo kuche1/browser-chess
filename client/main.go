a7f384bde1be391a9ef4ff5affb0af2c4494b2d4
kuche1 <znamsiparolata4esno@abv.bg>
2026-04-01 22:43:06 +0300

    update

--- client/main.go
+++ client/main.go
@@ -1,7 +1,22 @@
 package client
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 func Main() {
 	fmt.Printf("client: hi\n")
+
+	sceneBoard := NewSceneBoard()
+
+	// ebiten.SetWindowSize(800, 600)
+
+	ebiten.SetWindowTitle("Chess")
+
+	if err := ebiten.RunGame(sceneBoard); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ client/scene_board.go
@@ -0,0 +1,21 @@
+package client
+
+import "github.com/hajimehoshi/ebiten/v2"
+
+type SceneBoard struct {
+}
+
+func NewSceneBoard() *SceneBoard {
+	return &SceneBoard{}
+}
+
+func (self *SceneBoard) Update() error {
+	return nil
+}
+
+func (self *SceneBoard) Draw(screen *ebiten.Image) {
+}
+
+func (self *SceneBoard) Layout(outsideWidth int, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
+}
